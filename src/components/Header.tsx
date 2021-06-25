5fab212a1e7bec77ffc995501818464246a6c34b
JP Barbosa <jp@jp7.com.br>
2021-06-25 13:43:20 -0300

    Add tasks counter to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_TASKS } from '../queries/tasks';
+import { TaskData } from '../types/tasks';
 
 export const Header: React.FC = () => {
+  const { data } = useQuery<TaskData>(GET_TASKS);
+
   return (
     <header>
-      <div>React Basic CRUD</div>
-      <div>With GraphQL And TypeScript</div>
+      <h1>
+        <div className="title">React Basic CRUD</div>
+        <div className="subtitle">With GraphQL And TypeScript</div>
+      </h1>
+      <div className="counter">{data?.tasks.length}</div>
     </header>
   );
 };

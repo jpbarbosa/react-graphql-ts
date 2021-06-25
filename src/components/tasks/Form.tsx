c4070862583bd08d4e2aade59f0af2397a8b6760
JP Barbosa <jp@jp7.com.br>
2021-06-25 13:46:12 -0300

    Add focus to form input text

--- src/components/tasks/Form.tsx
+++ src/components/tasks/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useForm } from '../../hooks/useForm';
 import { Task } from '../../types/tasks';
 
@@ -8,9 +8,11 @@ interface IProps {
 }
 
 export const TaskForm: React.FC<IProps> = ({ activeTask, handleAction }) => {
+  const focus = useRef<HTMLInputElement>(null);
   const { handleChange, handleSubmit, formState } = useForm<Task>(
     activeTask,
-    handleAction
+    handleAction,
+    focus
   );
 
   return (
@@ -21,6 +23,7 @@ export const TaskForm: React.FC<IProps> = ({ activeTask, handleAction }) => {
         value={formState.name}
         onChange={handleChange}
         placeholder="Task description"
+        ref={focus}
       />
       <input type="submit" />
     </form>

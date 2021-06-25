5e9c3cde5fe79392d2f51ec84bbeeec295a2170b
JP Barbosa <jp@jp7.com.br>
2021-06-25 11:37:04 -0300

    Organize structure and remove unnecessary files

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header>
+        <div>React Basic CRUD</div>
+        <div>With GraphQL And TypeScript</div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
